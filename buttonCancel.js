f215f24181ebcb5765a425fa62dd13c0833373c5
p32bL3263oRm <37227431+cypetrova@users.noreply.github.com>
2024-03-20 09:52:44 +0200

    Delete buttonCancel.js

--- buttonCancel.js
+++
@@ -1,6 +0,0 @@
-</td><td></td><td><button type="button" id="bCancel" onclick="javascript:bCancelClick();">Отказ</button></td>
-
-function CancelPerson() {
-    
-    document.getElementById("tPerson").style.display = 'none';
-}
